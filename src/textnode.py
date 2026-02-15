0109fab883eed6031b5d9a70ed5c03fcca5620db
djm858 <duncanjmcfarland@gmail.com>
2026-02-14 21:30:33 -0600

    add text_node_to_html_node

--- src/textnode.py
+++ src/textnode.py
@@ -1,3 +1,4 @@
+from htmlnode import LeafNode
 from enum import Enum
 
 class TextType(Enum):
@@ -23,3 +24,27 @@ class TextNode:
 
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+
+def text_node_to_html_node(text_node):
+    match text_node.text_type:
+        case TextType.TEXT:
+            return LeafNode(None, text_node.text)
+        case TextType.BOLD:
+            return LeafNode("b", text_node.text)
+        case TextType.ITALIC:
+            return LeafNode("i", text_node.text)
+        case TextType.CODE:
+            return LeafNode("code", text_node.text)
+        case TextType.LINK:
+            props = {"href": text_node.url}
+            return LeafNode("a", text_node.text, props)
+        case TextType.IMAGE:
+            props = {
+                    "src": text_node.url,
+                    "alt": text_node.text
+            }
+            return LeafNode("img", "", props)
+        case _:
+            raise Exception("unknown text type to convert to html")
+
+
